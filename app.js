db2fad199ac8b929690eec1fb89afd80f5b7fd1b
Tivotal <121335284+Tivotal@users.noreply.github.com>
2025-02-03 10:20:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/* Created by Tivotal */
+
+var swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
